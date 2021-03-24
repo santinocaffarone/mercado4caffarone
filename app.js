92d0be0931cb900b238d8ad231b944b5bd71bbb8
santinocaffarone <santinocaffarone@mac.com>
2021-03-24 20:11:55 -0300

    caffarone-primer commit

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Example app listening on port '+ port);
 });
+
+
+// Paleta de colores 
+/*Amarillo: #EAC926;
+Azul: #03264C;
+Celeste: #1259c3;
+Gris oscuro: #666;
+Gris claro: #ebebeb;
+Verde: #39b54a;*/

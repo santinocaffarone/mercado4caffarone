bb44dd11a3bbac879aa5793164a1150370d27a12
santinocaffarone <santinocaffarone@mac.com>
2021-04-04 16:31:05 -0300

    caffarone - C06 y C07

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const port = 3000
 const path = require('path');
 app.use(express.static('public'));
 
+app.listen(port, () => {
+    console.log('Example app listening on port '+ port);
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.listen(port, () => {
-    console.log('Example app listening on port '+ port);
+app.get('/register/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
 

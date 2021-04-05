ed72e554262e66007f5dc6d47c7b157ddf042959
santinocaffarone <santinocaffarone@mac.com>
2021-04-05 19:21:33 -0300

    caffarone - preparacipn Heroku

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+//const port = 3000
+const puerto = process.env.PORT
 const path = require('path');
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log('Example app listening on port '+ port);
+
+app.listen(puerto || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
 });
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));

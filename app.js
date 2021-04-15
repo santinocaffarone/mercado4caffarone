9640058372c65d3b5f501d2b962d0439f8d56460
santinocaffarone <santinocaffarone@mac.com>
2021-04-15 19:10:37 -0300

    caffarone - con partials

--- app.js
+++ app.js
@@ -3,31 +3,24 @@ const app = express()
 //const port = 3000
 const puerto = process.env.PORT
 const path = require('path');
+
 app.use(express.static('public'));
-
-
+app.set('view engine', 'ejs'); //hay que instalarlos con "npm i ejs"
 app.listen(puerto || 3000, function() {
-    console.log("Servidor corriendo en el puerto 3000");
+    console.log("Servidor corriendo en el puerto ");
 });
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.render('home');
 });
 
 app.get('/register/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
+    res.render('register');
 });
 
 app.get('/login/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
+    res.render('login');
 });
 
 
-// Paleta de colores 
-/*Amarillo: #EAC926;
-Azul: #03264C;
-Celeste: #1259c3;
-Gris oscuro: #666;
-Gris claro: #ebebeb;
-Verde: #39b54a;*/
